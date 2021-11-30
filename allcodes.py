39a8158b34fcaf3527a8a88d4696511f8d55ab73
Harshit Sharma <66188432+harshitsharma52@users.noreply.github.com>
2021-11-30 22:59:32 +0530

    digit image and polarisation

--- allcodes.py
+++ allcodes.py
@@ -7,3 +7,12 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import cross_val_score
 
 mnist = fetch_openml('mnist_784')
+x, y = mnist['data'], mnist['target']
+
+some_digit = x.to_numpy()[36001]
+some_digit_image = some_digit.reshape(28, 28)  # let's reshape to plot it
+
+plt.imshow(some_digit_image, cmap=matplotlib.cm.binary,
+           interpolation='nearest')
+plt.axis("off")
+plt.show()
